54770bb934ff733b0e572a011dbdb62db2cf9b90
dpliakos <dimitrispl96@gmail.com>
2018-09-24 14:59:10 +0300

    create dummy components for two opportunities states

---
+++ src/components/components/opportunities-guest/opportunities-guest.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      Access denied!  <br/>
+      Become a member first
+    </div>
+  )
+}
---
+++ src/components/components/opportunities-member/opportunities-member.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      Opportunties page as a logged in user!
+    </div>
+  )
+}
