62c09e2e6a0246ee1e24c186d3fd32d4f2070595
dpliakos <dimitrispl96@gmail.com>
2018-09-27 12:29:08 +0300

    remove old code from layout2col component

--- src/components/layouts/layout-2col/layout-2col.js
+++ src/components/layouts/layout-2col/layout-2col.js
@@ -33,33 +33,4 @@ const Layout2Col = (props) => {
   )
 }
 
-
-
-// class Layout2Cols extends React.Component {
-//
-//   constructor(props) {
-//     super(props);
-//     this.baseClass = 'layout-2col';
-//     this.items = React.Children.map(this.props.children, (child) => {
-//       const className = `${child.props.className} ${this.baseClass}__col`;
-//       const props = { ...child.props, className: className };
-//       const newChild = React.cloneElement(child, props);
-//       return newChild;
-//     });
-//   }
-//
-//   render() {
-//     let classes = [this.baseClass ];
-//     if (this.props.horizontalGutters) classes.push(`${this.baseClass}--with-horizontal-gutters`);
-//     if (this.props.verticalGutters) classes.push(`${this.baseClass}--with-vertical-gutters`);
-//     const classString = classes.join(" ")
-//
-//     return(
-//     <div className={classString}>
-//          {this.items}
-//       </div>
-//     );
-//   }
-// }
-
 export default Layout2Col;
