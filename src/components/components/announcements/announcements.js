11026a526df9fef2e409cbaedc707ffe518c9dbc
dpliakos <dimitrispl96@gmail.com>
2018-10-16 01:25:28 +0300

    leave a todo

--- src/components/components/announcements/announcements.js
+++ src/components/components/announcements/announcements.js
@@ -3,6 +3,7 @@ import {graphql} from 'gatsby';
 import { ExternalResourceList } from './../external-resource-teaser/external-resource-teaser';
 
 export default (props) => {
+  // TODO: add announcements
   const items = [];
 
   return (
