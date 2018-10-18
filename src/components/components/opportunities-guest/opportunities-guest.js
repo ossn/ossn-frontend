f2950f2e4ecc84c4d3de3fb29258552d4024c2f4
dpliakos <dimitrispl96@gmail.com>
2018-10-18 18:46:13 +0300

    Update markup for not a member opportunities page. Fix #35

--- src/components/components/opportunities-guest/opportunities-guest.js
+++ src/components/components/opportunities-guest/opportunities-guest.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default (props) => {
   return (
     <div>
-      Access denied!  <br/>
-      Become a member first
+      <a href="#"> <div className="button">  Become a member first </div> </a>
     </div>
-  )
+  );
 }
