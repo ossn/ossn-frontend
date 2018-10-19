fe1f63c45b57cc436b85684b933d70e46ad401ef
dpliakos <dimitrispl96@gmail.com>
2018-10-19 17:30:22 +0300

    WIP theme organizations layout

--- src/components/components/organizations-list/organizations-list.js
+++ src/components/components/organizations-list/organizations-list.js
@@ -2,13 +2,15 @@ import React from 'react';
 import OrganizationTeaser from './../organization-teaser/organization-teaser';
 import { graphql } from "gatsby"
 
+import './organizations-list.scss';
+
 export default (props) => {
   const organizations = props.organizations.map((node, i)=>{
     return <OrganizationTeaser organization={node.org} key={i} />
   });
 
   return (
-    <div className="test">
+    <div className="organizations-list">
       {organizations}
     </div>
   );
