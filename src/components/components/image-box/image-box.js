e15676dd2b0dd5eef6dc7bd80c3608645da2ee47
iro <iropal14@gmail.com>
2018-10-25 15:42:20 +0300

    Adds custom grid layout.

--- src/components/components/image-box/image-box.js
+++ src/components/components/image-box/image-box.js
@@ -3,27 +3,35 @@ import React from 'react';
 
 // Local modules.
 import './image-box.scss'
-import Image from './images/panel-typewriter.jpg'
+// import Image from './images/panel-typewriter.jpg'
 
 export default (props) => {
+  const baseClass = 'image-box';
+
+
   const titleLargeTop = props.titleLargeTop ? <span>{props.titleLargeTop}</span> : '';
   const titleSmall = props.titleSmall ? <span>{props.titleSmall}</span> : '';
   const titleLargeBottom = props.titleLargeBottom ? <span>{props.titleLargeBottom}</span> : '';
   // const image = props.image ? props.image : Image;
-  const image = Image;
-  const quotedText = props.quotedText;
-  const quoteSignature = props.quoteSignature;
-  const link = props.link;
+  // const image = Image;
+  // const quotedText = props.quotedText;
+  // const quoteSignature = props.quoteSignature;
+  // const link = props.link;
+
+  // handle component classes. Add the variations found in props.
+  let classes = [baseClass];
+  if (props.tall) classes.push(`${baseClass}--tall`);
+  const classString = classes.join(" ");
 
   return (
-    <div>
-      <img src={image} alt="" className=""/>
-      <h3>
-        {titleLargeTop}
-        {titleSmall}
-        {titleLargeBottom}
-      </h3>
-      <div>
+    <div className={classString}>
+      {/*<img src={image} alt="" className="image-box__image"/>*/}
+      <div className="image-box__text">
+        <h3>
+          {titleLargeTop}
+          {titleSmall}
+          {titleLargeBottom}
+        </h3>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import OrganizationList from './../components/components/organizations-list/orga
 import BecomeMember from './../components/components/home-become-member/home-become-member';
 import ImageBox from './../components/components/image-box/image-box';
 import BlockquoteBox from './../components/components/blockquote-box/blockquote-box';
+import '../components/layouts/layout-custom-grid/layout-custom-grid.scss'
 
 const IndexPage = (props) => {
   return (
@@ -19,12 +20,25 @@ const IndexPage = (props) => {
         <div>
           <div >
             <div>
-              <div>
-                <ImageBox titleLargeTop="Read" titleSmall="our updated" titleLargeBottom="blog!" image="./../components/components/image-box/image-box/images/panel-typewriter.jpg"/>               <ImageBox titleLargeTop="MozFest is over :-(" titleSmall="But you can re-live it with our photos!"/>
-                <ImageBox titleLargeTop="Fixme" titleSmall="New kid on the block!"/>
-                <ImageBox titleSmall="See the all-new" titleLargeBottom="Opportunities!"/>
-                <ImageBox titleLargeTop="Listen" titleSmall="to our" titleLargeBottom="podcast!"/>
-                <BlockquoteBox blockquote="In open source, we feel strongly that to really do something well, you have to get a lot of people involved." author="Linus Torvalds" />
+              <div className="layout-custom-grid">
+                <div className="layout-custom-grid__item layout-custom-grid__item--1">
+                  <ImageBox tall titleLargeTop="Read" titleSmall="our updated" titleLargeBottom="blog!" image="./../components/components/image-box/image-box/images/panel-typewriter.jpg"/>
+                </div>
+                <div className="layout-custom-grid__item layout-custom-grid__item--2">
+                  <ImageBox titleSmall="See the all-new" titleLargeBottom="Opportunities!"/>
+                </div>
+                <div className="layout-custom-grid__item layout-custom-grid__item--3">
+                  <ImageBox tall titleLargeTop="Listen" titleSmall="to our" titleLargeBottom="podcast!"/>
+                </div>
+                <div className="layout-custom-grid__item layout-custom-grid__item--4">
+                  <ImageBox titleLargeTop="Fixme" titleSmall="New kid on the block!"/>
+                </div>
+                <div className="layout-custom-grid__item layout-custom-grid__item--5">
+                  <ImageBox titleLargeTop="MozFest is over :-(" titleSmall="But you can re-live it with our photos!"/>
+                </div>
+                <div className="layout-custom-grid__item layout-custom-grid__item--6">
+                  <BlockquoteBox blockquote="In open source, we feel strongly that to really do something well, you have to get a lot of people involved." author="Linus Torvalds" />
+                </div>
               </div>
             </div>
           </div>
