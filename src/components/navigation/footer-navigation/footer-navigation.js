96ef42da6236ecae9c1e08d16b4394b9a27f1496
Alexandros Dorodoulis <aldor94@gmail.com>
2018-10-30 15:05:05 +0100

    fix: Fix footer navigation links

--- src/components/navigation/footer-navigation/footer-navigation.js
+++ src/components/navigation/footer-navigation/footer-navigation.js
@@ -1,36 +1,44 @@
 // external modules
+import { Link } from 'gatsby';
 import React from 'react';
-import {Link} from 'gatsby';
 
-class Navigation extends React.Component {
-  render() {
-    return(
-      <section className="footer__section footer__section--navigation">
-        <h5 className="footer__heading"> Navigate </h5>
-        <ul className="footer__list">
-          <li>
-            <Link className="footer__link" link="/find-club">Clubs</Link>
-          </li>
-          <li>
-            <Link className="footer__link" link="/members">Members</Link>
-          </li>
-          <li>
-            <Link className="footer__link" link="/contribute">Opportunities</Link>
-          </li>
-          {/*TODO Add correct link*/}
-          <li>
-            <Link className="footer__link" link="/member">Login/Signup</Link>
-          </li>
-          <li>
-            <Link className="footer__link" link="/organizations">Affiliations</Link>
-          </li>
-          <li>
-            <Link className="footer__link" link="/about-us">About us</Link>
-          </li>
-        </ul>
-      </section>
-    )
-  }
-}
+const Navigation = () => (
+  <section className="footer__section footer__section--navigation">
+    <h5 className="footer__heading"> Navigate </h5>
+    <ul className="footer__list">
+      <li>
+        <Link className="footer__link" to="/find-club">
+          Clubs
+        </Link>
+      </li>
+      <li>
+        <Link className="footer__link" to="/members">
+          Members
+        </Link>
+      </li>
+      <li>
+        <Link className="footer__link" to="/contribute">
+          Opportunities
+        </Link>
+      </li>
+      {/*TODO Add correct link*/}
+      <li>
+        <Link className="footer__link" to="/member">
+          Login/Signup
+        </Link>
+      </li>
+      <li>
+        <Link className="footer__link" to="/organizations">
+          Affiliations
+        </Link>
+      </li>
+      <li>
+        <Link className="footer__link" to="/about-us">
+          About us
+        </Link>
+      </li>
+    </ul>
+  </section>
+)
 
-export default Navigation;
+export default Navigation
