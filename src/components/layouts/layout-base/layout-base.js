aff7340d474badf4f66e95824608e4b0c0af2ece
Alexandros Dorodoulis <aldor94@gmail.com>
2018-10-30 23:25:32 +0100

    feat: Enable strict mode to ensure forward compatibility

--- src/components/layouts/layout-base/layout-base.js
+++ src/components/layouts/layout-base/layout-base.js
@@ -1,58 +1,58 @@
-// Styles
 import './../../base-styles/base/normalize.scss';
 import './../../base-styles/fonts/fonts.scss';
 import './layout-base.scss';
 
-// utils
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import fetch from 'node-fetch';
-import React, { memo } from 'react';
+import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { Helmet } from 'react-helmet';
 import { Provider } from 'react-redux';
 
-// Import local modules
 import GatsbyConfig from './../../../../gatsby-config';
 import { link, metadata } from './../../../settings';
 import store from './../../../store';
 import Footer from './../../components/footer/footer';
 import Header from './../../components/header/header';
 import SkipLink from './../../components/skip-link/skip-link';
+
 // import LayoutContained from "./../layout-contained/layout-contained";
 
 // TODO: Remove and fid title another way.
+
 const Basic = ({ children }) => {
   const client = new ApolloClient({
     link: new HttpLink({
       uri: 'https://07rqzpwj55.lp.gql.zone/graphql',
-      fetch: fetch,
+      fetch: fetch
     }),
-    cache: new InMemoryCache(),
-  })
+    cache: new InMemoryCache()
+  });
 
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <div>
-          <Helmet meta={metadata} link={link}>
-            <title>{GatsbyConfig.siteMetadata.title}</title>
-          </Helmet>
-          <SkipLink />
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
           <div>
-            <Header />
+            <Helmet meta={metadata} link={link}>
+              <title>{GatsbyConfig.siteMetadata.title}</title>
+            </Helmet>
+            <SkipLink />
+            <div>
+              <Header />
+            </div>
+            <div id="content" className="layout-base">
+              {children}
+            </div>
+            <footer>
+              <Footer />
+            </footer>
           </div>
-          <div id="content" className="layout-base">
-            {children}
-          </div>
-          <footer>
-            <Footer />
-          </footer>
-        </div>
-      </Provider>
-    </ApolloProvider>
-  )
-}
-
-export default memo(Basic)
+        </Provider>
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+};
+export default React.memo(Basic);
