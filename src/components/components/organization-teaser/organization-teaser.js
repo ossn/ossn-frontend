ce614f20f0492f21c6af4505e8b4323850ff2762
iro <iropal14@gmail.com>
2018-10-30 14:06:10 +0200

    Themes oraganization list for front page.

--- src/components/components/organization-teaser/organization-teaser.js
+++ src/components/components/organization-teaser/organization-teaser.js
@@ -4,6 +4,9 @@
 
 import React from 'react';
 
+// Style.
+import './organization-teaser.scss'
+
 const OrganizationTeaser = (props) => {
   if (!props.organization) {
     return <div> No organization found </div>;
@@ -13,8 +16,12 @@ const OrganizationTeaser = (props) => {
   const url = props.organization.link;
   const icon = props.organization.attachment.publicURL;
 
+  let classes = ['organization-teaser'];
+  if (props.className) classes.push(props.className);
+  let classString = classes.join(" ");
+
   return (
-    <a href={url} className={props.className || ''}>
+    <a href={url} className={classString} title={title} target="_blank">
       <img src={icon} alt={title} />
     </a>
   );
