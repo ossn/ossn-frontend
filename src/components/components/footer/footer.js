94946b688b95a59008fbd014c5fded51da0d5bd3
dpliakos <dimitrispl96@gmail.com>
2018-11-01 17:58:53 +0200

    Create the responsive list for the footer

--- src/components/components/footer/footer.js
+++ src/components/components/footer/footer.js
@@ -4,7 +4,8 @@
 
 // External modules.
 import React from 'react';
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
+import MediaQuery from 'react-responsive';
 
 // Local modules.
 import LayoutContained from './../../layouts/layout-contained/layout-contained';
@@ -13,51 +14,76 @@ import Navigation from './../../navigation/footer-navigation/footer-navigation';
 // Styles.
 import './footer.scss';
 
-const Footer = (props) => {
+const Footer = props => {
   return (
     <div className="footer">
       <LayoutContained className="footer__inner">
         <nav className="footer__primary">
-          <Link to="/" className="footer__primary-logo" activeClassName="is-active">
-            <span className="footer__ossn-logo">Open Source Student Network</span>
+          <Link
+            to="/"
+            className="footer__primary-logo"
+            activeClassName="is-active"
+          >
+            <span className="footer__ossn-logo">
+              Open Source Student Network
+            </span>
             <span className="footer__mozilla-logo">Mozilla</span>
           </Link>
 
           <div className="footer__sections">
-            <Navigation />
-            <section className="footer__section footer__section--social">
-              <h5 className="footer__heading"> Keep in touch </h5>
-              <ul className="footer__list">
-                <li>
-                  {/*TODO add correct url*/}
-                  <a className="footer__link" href="/">Our blog</a>
-                </li>
-                <li>
-                  <a className="footer__link" href="/">Facebook page</a>
-                </li>
-                <li>
-                  <a  className="footer__link"href="/">Twitter page</a>
-                </li>
-              </ul>
-            </section>
+            <MediaQuery minWidth={768} className="footer__section-item">
+              <Navigation title="Navigation" links={footerNavigationList1} />
+              <Navigation title="Keep in touch" links={footerNavigationList2} />
+            </MediaQuery>
+            <MediaQuery maxWidth={767} className="footer__section-item">
+              <Navigation
+                title="Navigation"
+                isMobile
+                links={footerNavigationList1}
+              />
+              <Navigation
+                title="Keep in touch"
+                isMobile
+                links={footerNavigationList2}
+              />
+            </MediaQuery>
           </div>
         </nav>
 
         <nav className="footer__secondary">
           <div className="footer__legal">
             <p className="footer__license">
-              Portions of this content are ©1998–2018 by individual mozilla.org contributors.
-              Content available under a <a rel="license" href="https://www.mozilla.org/foundation/licensing/website-content/">Creative Commons license</a>.
+              Portions of this content are ©1998–2018 by individual mozilla.org
+              contributors. Content available under a{' '}
+              <a
+                rel="license"
+                href="https://www.mozilla.org/foundation/licensing/website-content/"
+              >
+                Creative Commons license
+              </a>
+              .
             </p>
             <ul className="footer__terms">
               <li>
-                <a rel="nofollow" href="https://www.mozilla.org/privacy/websites/">Website Privacy Notice</a>
+                <a
+                  rel="nofollow"
+                  href="https://www.mozilla.org/privacy/websites/"
+                >
+                  Website Privacy Notice
+                </a>
               </li>
               <li>
-                <a rel="nofollow" href="https://www.mozilla.org/privacy/websites/#cookies">Cookies</a>
+                <a
+                  rel="nofollow"
+                  href="https://www.mozilla.org/privacy/websites/#cookies"
+                >
+                  Cookies
+                </a>
               </li>
               <li>
-                <a rel="nofollow" href="https://www.mozilla.org/about/legal/">Legal</a>
+                <a rel="nofollow" href="https://www.mozilla.org/about/legal/">
+                  Legal
+                </a>
               </li>
             </ul>
           </div>
@@ -68,3 +94,49 @@ const Footer = (props) => {
 };
 
 export default Footer;
+
+// trivially added link list.
+const footerNavigationList1 = [
+  {
+    title: 'Clubs',
+    target: '/find-club'
+  },
+  {
+    title: 'Members',
+    target: '/members'
+  },
+  {
+    title: 'Opportunities',
+    ttarget: '/contribute'
+  },
+  {
+    title: 'Log In/SignUp',
+    target: '/'
+  },
+  {
+    title: 'Affiliations',
+    target: '/affiliations'
+  },
+  {
+    title: 'About Us',
+    target: '/about'
+  }
+];
+
+const footerNavigationList2 = [
+  {
+    title: 'Our blog',
+    target: '',
+    external: true
+  },
+  {
+    title: 'Facebook page',
+    target: '',
+    external: true
+  },
+  {
+    title: 'Twitter page',
+    target: '',
+    external: true
+  }
+];
--- src/components/navigation/footer-navigation/footer-navigation.js
+++ src/components/navigation/footer-navigation/footer-navigation.js
@@ -1,44 +1,117 @@
+/*
+  Constructs a collapsible list of entries at the mobile.
+
+  props:
+    title: the title of the list.
+    links: the list of the links to be displayed.
+
+  a link attribute = {
+    title (str): the text to be displayed
+    target (str): the `to` or `href` attribute of the `Link` or `a` element.
+    external (boolean):  if true, the component will be `<a>` else `<Link>`.
+  }
+*/
+
 // external modules
 import { Link } from 'gatsby';
 import React from 'react';
 
-const Navigation = () => (
-  <section className="footer__section footer__section--navigation">
-    <h5 className="footer__heading"> Navigate </h5>
-    <ul className="footer__list">
-      <li>
-        <Link className="footer__link" to="/find-club">
-          Clubs
-        </Link>
-      </li>
-      <li>
-        <Link className="footer__link" to="/members">
-          Members
-        </Link>
-      </li>
-      <li>
-        <Link className="footer__link" to="/contribute">
-          Opportunities
-        </Link>
-      </li>
-      {/*TODO Add correct link*/}
-      <li>
-        <Link className="footer__link" to="/member">
-          Login/Signup
-        </Link>
-      </li>
-      <li>
-        <Link className="footer__link" to="/organizations">
-          Affiliations
-        </Link>
-      </li>
-      <li>
-        <Link className="footer__link" to="/about-us">
-          About us
-        </Link>
-      </li>
-    </ul>
-  </section>
-)
+// returns just the title
+const SimpleHeader = props => {
+  return <div> {props.title} </div>;
+};
 
-export default Navigation
+// if the screen is mobile, wraps the SimpleHeader with a button to handle the
+// click action
+const ResponsiveHeader = props => {
+  const icon = props.isOpen ? '-' : '+';
+  return (
+    <button
+      className="footer__heading footer__heading--button"
+      onClick={props.onClick}
+    >
+      <SimpleHeader title={props.title} />
+      <span>{icon}</span>
+    </button>
+  );
+};
+
+// A link that points inside the site. Uses the `Link`  component.
+const InternalLink = props => {
+  return (
+    <Link className="footer__link" to={props.link.target}>
+      {props.link.title}
+    </Link>
+  );
+};
+
+// A link that points outside the site. Users the `a` component.
+const ExternalLink = props => {
+  return (
+    <a className="footer__link" href={props.link.target}>
+      {props.link.title}
+    </a>
+  );
+};
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    const isMobile = this.props.isMobile;
+    this.state = {
+      isOpen: false,
+      isMobile: isMobile
+    };
+  }
+
+  handleClick = event => {
+    const snapshot = { ...this.state };
+    if (!snapshot.isMobile) return;
+    this.setState({
+      isOpen: !snapshot.isOpen
+    });
+  };
+
+  parseLinks = linkList => {
+    if (!linkList) return [];
+    const list = linkList.map((link, i) => {
+      const cmp = link.external ? (
+        <ExternalLink link={link} key={i} />
+      ) : (
+        <InternalLink link={link} key={i} />
+      );
+
+      return <li key={i}>{cmp}</li>;
+    });
+
+    return list;
+  };
+
+  render() {
+    const snapshot = { ...this.state };
+    const links = this.parseLinks(this.props.links);
+    const header = snapshot.isMobile ? (
+      <ResponsiveHeader
+        onClick={() => {
+          this.handleClick();
+        }}
+        title={this.props.title}
+        isOpen={snapshot.isOpen}
+      />
+    ) : (
+      <SimpleHeader title={this.props.title} />
+    );
+
+    return (
+      <section className="footer__section footer__section--navigation footer__section--mobile">
+        <h5 className="footer__heading">{header}</h5>
+        <ul
+          className="footer__list"
+          hidden={!snapshot.isOpen && snapshot.isMobile}
+        >
+          {links}
+        </ul>
+      </section>
+    );
+  }
+}
