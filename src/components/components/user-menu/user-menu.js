ebf8701fe3ef6cd4aca30c5e2910637db6107713
iro <iropal14@gmail.com>
2018-11-01 13:58:07 +0200

    Updates node modules, add real content for trainig resources and tools.

--- src/components/components/user-menu/user-menu.js
+++ src/components/components/user-menu/user-menu.js
@@ -1,55 +1,56 @@
+/* eslint-disable */
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { User } from 'react-feather';
 
-import LogedInMenu from './logged-in-user';
+import LoggedInMenu from './logged-in-user';
 import NotLoggedInMenu from './not-logged-in-user';
 
-import {mapUserToProps} from './../../../utils/redux-utils';
+import { mapUserToProps } from './../../../utils/redux-utils';
 
-import './user-menu.scss'
+import './user-menu.scss';
 
 class UserMenu extends React.Component {
   constructor(props) {
     super(props);
-		this.popup = React.createRef();
-		this.hideMenu = this.handleOutsideClick.bind(this);
-		this.state = {
+    this.popup = React.createRef();
+    this.hideMenu = this.handleOutsideClick.bind(this);
+    this.state = {
       option: 'login',
       open: false
-    }
+    };
   }
 
   // puts an event listener for the UI handling (not that unsafe)
-	UNSAFE_componentWillMount() {
+  componentDidMount() {
     if (typeof document !== 'undefined')
-		  document.addEventListener('mousedown', this.handleOutsideClick, false);
-	}
+      document.addEventListener('mousedown', this.handleOutsideClick, false);
+  }
 
   // remove the listener in absence of the component
-	ComponentWillUnmount() {
+  ComponentWillUnmount() {
     if (typeof document !== 'undefined')
-		  document.removeEventListener('mousedown', this.handleOutsideClick, false);
-	}
+      document.removeEventListener('mousedown', this.handleOutsideClick, false);
+  }
 
   handleOpen = () => {
-    this.setState({open: !this.state.open});
-  }
+    this.setState({ open: !this.state.open });
+  };
 
-  handleOption = (newOption) => {
-    this.setState({option: newOption});
-  }
+  handleOption = newOption => {
+    this.setState({ option: newOption });
+  };
 
-	handleOutsideClick = (event) => {
-		if (this.state.open && !this.popup.current.contains(event.target)) {
-			this.setState({open: false});
-			return;
-		}
-	}
+  handleOutsideClick = event => {
+    if (this.state.open && !this.popup.current.contains(event.target)) {
+      this.setState({ open: false });
+    }
+  };
 
   render() {
     // copy of the state
-    const snapshot = {...this.state};
+    const snapshot = { ...this.state };
 
     // menu place holder
     let content = <div> </div>;
@@ -58,26 +59,24 @@ class UserMenu extends React.Component {
     let extraContent = '';
 
     if (this.props.user.loggedIn) {
-      extraContent =  <span> logged in </span>;
+      extraContent = <span> logged in </span>;
     }
 
     // decide which menu to show
     if (this.props.user.loggedIn) {
-      content = <LogedInMenu />;
-    } else  {
+      content = <LoggedInMenu />;
+    } else {
       content = <NotLoggedInMenu />;
     }
 
-    return(
-      <div ref={ this.popup} className="user-menu">
-        <div onClick={this.handleOpen} className="button button--header">
+    return (
+      <div className="user-menu">
+        <button onClick={this.handleOpen} onKeyPress={this.handleOpen} className="button button--header">
           <User className="user-menu__icon" />
           {snapshot.open ? 'close' : 'Login/Signup'}
           {extraContent}
-        </div>
-        <div className="user-menu__popup">
-          { snapshot.open ?  content : <div></div>  }
-        </div>
+        </button>
+        <div className="user-menu__popup">{snapshot.open ? content : ''}</div>
       </div>
     );
   }
