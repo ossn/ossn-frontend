47fb588a1289aebe56e7e29e96e085812b7065d7
iro <iropal14@gmail.com>
2018-11-05 18:06:27 +0200

    Adds layout scroll to opportunities slices and various fixes on leadrs corner and opprtunities page.

--- src/components/components/contributing-tools/contributing-tools.js
+++ src/components/components/contributing-tools/contributing-tools.js
@@ -5,10 +5,12 @@
 */
 import React from 'react';
 import { graphql } from 'gatsby';
+import MediaQuery from 'react-responsive';
 
 // Local modules.
 import ExternalLinkBox from './../external-link-box/external-link-box';
 import Layout3Col from './../../layouts/layout-3col/layout-3col';
+import LayoutScroll from './../../layouts/layout-scroll/layout-scroll';
 
 export const ContributingToolSet = props => {
   const tools = props.tools.edges.map((toolNode, i) => {
@@ -23,9 +25,15 @@ export const ContributingToolSet = props => {
   return (
     <div>
       <h2> Tools for contributing code to open source projects </h2>
-      <Layout3Col horizontalGutters verticalGutters>
-        {tools}
-      </Layout3Col>
+      <MediaQuery maxWidth={767}>
+        <LayoutScroll>{tools}</LayoutScroll>
+      </MediaQuery>
+
+      <MediaQuery minWidth={768}>
+        <Layout3Col horizontalGutters verticalGutters>
+          {tools}
+        </Layout3Col>
+      </MediaQuery>
     </div>
   );
 };
--- src/components/layouts/layout-scroll/layout-scroll.js
+++ src/components/layouts/layout-scroll/layout-scroll.js
@@ -5,13 +5,24 @@ import React, { memo } from 'react';
 const LayoutScroll = props => {
   const baseClass = 'layout-scroll';
 
+  // add the `layout-scroll__item` class to every child.
+  const children = React.Children.map(props.children, child => {
+    const className = `${
+      child.props.className || '' ? child.props.className : ''
+    } ${baseClass}__item`;
+    const props = { ...child.props, className: className };
+    const newChild = React.cloneElement(child, props);
+    return newChild;
+  });
+
   // Handle extra classes. Add the classes found in props.
   let classes = [baseClass];
   if (props.className) classes.push(props.className);
+  if (props.stretchItems) classes.push(`${baseClass}--stretch-items`);
 
   const classString = classes.join(' ');
 
-  return <div className={classString}>{props.children}</div>;
+  return <div className={classString}>{children}</div>;
 };
 
 export default memo(LayoutScroll);
