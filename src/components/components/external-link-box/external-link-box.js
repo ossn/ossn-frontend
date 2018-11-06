143c69eb2de7617979939011dc5c134ff556ce47
iro <iropal14@gmail.com>
2018-11-06 14:09:13 +0200

    Themes external link box component and adds interaction to shadow box.

--- src/components/components/external-link-box/external-link-box.js
+++ src/components/components/external-link-box/external-link-box.js
@@ -14,20 +14,21 @@ export const ExternalLinkBox = props => {
   const resource = props.resource;
   const title = resource.title;
   const target = resource.link;
+  const description = resource.description;
   const image = resource.attachment.publicURL;
 
   const classes = [props.className, 'external-link-box'];
   return (
     <div className={classes.join(' ')}>
       <a href={target} className="external-link-box__wrapper-link">
-        <ShadowBox zeroPadding>
+        <ShadowBox zeroPadding className="external-link-box__link-inner">
           <div className="external-link-box__image-wrapper">
             <img src={image} alt={title} className="external-link-box__image" />
           </div>
           <div className="external-link-box__text">
             <div className="external-link-box__title"> {title} </div>
             <div className="external-link-box__url">
-              <Link size={12} /> {target}{' '}
+              <Link size={12} /> {description}
             </div>
           </div>
         </ShadowBox>
