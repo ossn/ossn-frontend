aa736b91da93bf8109b6cbb629f9cc4ba4247888
iro <iropal14@gmail.com>
2018-11-08 14:01:01 +0200

    Themes front page image boxes and blocquote component..

--- src/components/components/blockquote-box/blockquote-box.js
+++ src/components/components/blockquote-box/blockquote-box.js
@@ -1,7 +1,6 @@
-// External modules.
 import React from 'react';
 
-// Local modules.
+import Shape from './../shape/shape';
 import './blockquote-box.scss';
 
 export default props => {
@@ -9,11 +8,42 @@ export default props => {
   const author = props.author;
 
   return (
-    <blockquote>
-      <p>{blockquote}</p>
-      <footer>
-        <cite>{author}</cite>
-      </footer>
-    </blockquote>
+    <div className="blockquote-box">
+      <blockquote className="blockquote-box__blockquote">
+        <p className="blockquote-box__text">
+          <svg
+            className="blockquote-box__svg blockquote-box__svg--before"
+            xmlns="http://www.w3.org/2000/svg"
+            width="84"
+            height="65"
+            viewBox="-0.2 0.1 84.2 64.5"
+          >
+            <title>“</title>
+            <path
+              d="M33.4 46c-2.2 9.6-8.6 18.1-18.7 18.1 -8.4 0-14.4-5.5-14.4-14.4 0-19.3 13.5-36 30.2-49l10.1 10.4c-5.5 3.7-16.1 13.2-18.7 21C29.4 33.3 35.6 36.4 33.4 46zM76 46c0 9.8-8.6 18.1-18.7 18.1 -8.4 0-14.4-5.5-14.4-14.4 0-19.3 13.5-36 30.2-49l10.1 10.4c-5.5 3.7-16.1 13.2-18.7 21C72 33.3 76 39.4 76 46z"
+              fill="#FFF"
+            />
+          </svg>
+          {blockquote}
+          <svg
+            className="blockquote-box__svg blockquote-box__svg--after"
+            xmlns="http://www.w3.org/2000/svg"
+            width="84"
+            height="65"
+            viewBox="-0.2 0.1 84.2 64.5"
+          >
+            <title>“</title>
+            <path
+              d="M50.1 18.9c2.2-9.6 8.6-18.1 18.7-18.1 8.4 0 14.4 5.5 14.4 14.4 0 19.3-13.5 36-30.2 49l-10.1-10.4c5.5-3.7 16.1-13.2 18.7-21C54.1 31.6 47.9 28.6 50.1 18.9zM7.5 18.9c0-9.8 8.6-18.1 18.7-18.1 8.4 0 14.4 5.5 14.4 14.4 0 19.3-13.5 36-30.2 49L0.3 53.8c5.5-3.7 16.1-13.2 18.7-21C11.5 31.6 7.5 25.6 7.5 18.9z"
+              fill="#FFF"
+            />
+          </svg>
+        </p>
+        <Shape waveLarge seafoamBlue className="blockquote-box__wave" />
+        <footer>
+          <cite className="blockquote-box__cite">{author}</cite>
+        </footer>
+      </blockquote>
+    </div>
   );
 };
--- src/components/components/image-box/image-box.js
+++ src/components/components/image-box/image-box.js
@@ -1,10 +1,12 @@
 /*
-  The news tiles.
-  Appears at / (home page)
-*/
+ The news tiles.
+ Appears at / (home page)
+ */
 
 // External modules.
+import { Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
 
 // Local modules.
 import './image-box.scss';
@@ -13,33 +15,46 @@ import './image-box.scss';
 export default props => {
   const baseClass = 'image-box';
 
-  const titleLargeTop = props.titleLargeTop ? (
-    <span>{props.titleLargeTop}</span>
+  const resource = props.resource[0];
+  const titleLargeTop = resource.titleLargeTop ? (
+    <span className="image-box__title-large image-box__title-large--top">
+      {resource.titleLargeTop}
+    </span>
   ) : (
     ''
   );
-  const titleSmall = props.titleSmall ? <span>{props.titleSmall}</span> : '';
-  const titleLargeBottom = props.titleLargeBottom ? (
-    <span>{props.titleLargeBottom}</span>
+  const titleSmall = resource.titleSmall ? (
+    <span className="image-box__title-small">{resource.titleSmall}</span>
   ) : (
     ''
   );
-  // const image = props.image ? props.image : Image;
-  // const image = Image;
-  // const quotedText = props.quotedText;
-  // const quoteSignature = props.quoteSignature;
-  // const link = props.link;
-
-  // handle component classes. Add the variations found in props.
+  const titleLargeBottom = resource.titleLargeBottom ? (
+    <span className="image-box__title-large image-box__title-large--top">
+      {resource.titleLargeBottom}
+    </span>
+  ) : (
+    ''
+  );
+  const image = resource.image ? resource.image : '';
+  // Handle component classes. Add the variations found in props.
   let classes = [baseClass];
-  if (props.tall) classes.push(`${baseClass}--tall`);
+  if (props.large) classes.push(`${baseClass}--large`);
+  if (props.centered) classes.push(`${baseClass}--centered`);
+  if (props.offset) classes.push(`${baseClass}--offset`);
+  if (props.end) classes.push(`${baseClass}--end`);
+  if (props.orange) classes.push(`${baseClass}--orange`);
+  if (props.light) classes.push(`${baseClass}--light`);
+  if (props.className) classes.push(props.className);
   const classString = classes.join(' ');
 
-  return (
+  const content = (
     <div className={classString}>
-      {/*<img src={image} alt="" className="image-box__image"/>*/}
+      <div className="image-box__image">
+        <Img fluid={image.src.childImageSharp.fluid} />
+      </div>
+
       <div className="image-box__text">
-        <h3>
+        <h3 className="image-box__title">
           {titleLargeTop}
           {titleSmall}
           {titleLargeBottom}
@@ -47,4 +62,26 @@ export default props => {
       </div>
     </div>
   );
+
+  // Use flag for the link generation.
+  const url = resource.link;
+  const isInternalLink = resource.internalLink;
+  if (isInternalLink) {
+    return (
+      <Link to={url} className="image-box__link">
+        {content}
+      </Link>
+    );
+  } else {
+    return (
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="image-box__link"
+      >
+        {content}
+      </a>
+    );
+  }
 };
