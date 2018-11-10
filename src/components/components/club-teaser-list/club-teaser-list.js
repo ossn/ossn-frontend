ca4e68717c125484f4089f7a386606eab7741ad8
dpliakos <dimitrispl96@gmail.com>
2018-11-10 15:25:33 +0200

    Finish the club teaser

--- src/components/components/club-teaser-list/club-teaser-list.js
+++ src/components/components/club-teaser-list/club-teaser-list.js
@@ -17,24 +17,23 @@ export const ClubTeaser = props => {
   const title = props.club.title;
   const subtitle = props.club.subtitle;
   const image = props.club.imageUrl;
+  const classes = props.className ? props.className : '';
 
   return (
-    <div className={`${props.className} teaser-box`}>
-      <ShadowBox>
-        <div className="teaser-box__inner">
-          <div className="club-teaser__image-wrapper">
-            <img src={image} alt={title} className="club-teaser__image" />
-          </div>
-          <div className="club-teaser__text">
-            <span className="title title--x-small club-teaser__title">
-              {title}
-            </span>
-            <span className="club-teaser__description"> {subtitle} </span>
-            <div className="button button--x-small club-teaser__button">
-              <Link to="/club" className="club-teaser__link">
-                Club page <ChevronRight size={16} className="icon" />
-              </Link>
-            </div>
+    <div className={`${classes} club-teaser`}>
+      <ShadowBox className="club-teaser__inner">
+        <div className="club-teaser__image-wrapper">
+          <img src={image} alt={title} className="club-teaser__image" />
+        </div>
+        <div className="club-teaser__text">
+          <span className="title title--x-small club-teaser__title">
+            {title}
+          </span>
+          <span className="club-teaser__description"> {subtitle} </span>
+          <div className="button button--x-small club-teaser__button">
+            <Link to="/club" className="club-teaser__link">
+              Club page <ChevronRight size={16} className="icon" />
+            </Link>
           </div>
         </div>
       </ShadowBox>
