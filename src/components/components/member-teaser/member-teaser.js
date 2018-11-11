c736bb43882b997ba22e109a440e0afa507a00eb
dpliakos <dimitrispl96@gmail.com>
2018-11-11 16:24:34 +0200

    feat: Theme member teaser.'

--- src/components/components/member-teaser/member-teaser.js
+++ src/components/components/member-teaser/member-teaser.js
@@ -1,23 +1,50 @@
+/*
+  Is the the preview of a member profile.
+
+  props:
+    member (object): the member to be shown.
+*/
 import React from 'react';
+import { Users } from 'react-feather';
+
 import Member from './../member/member';
 import ShadowBox from './../shadow-box/shadow-box';
-
 // utils
 import { returnKeyCheck } from './../../../utils/accessibility';
+
+import './member-teaser.scss';
 
 const MemberTeaser = props => {
   const handleClick = () => {
     if (props.onClick) props.onClick(props.id);
   };
 
+  const name = `${props.member.firstName} ${props.member.lastName}`;
+  const imageUrl =
+    props.member.imageUrl ||
+    'http://assets.nydailynews.com/polopoly_fs/1.2479149.1451350340!/img/httpImage/image.jpg_gen/derivatives/article_750/motorhead29n-2-web.jpg';
+  const clubString = 'RIT Linux Users Group';
+
+  // Whean the popup is open, the preview is loaded.
   let preview = <div />;
   if (props.open) {
     preview = <Member member={props.member} />;
   }
 
+  // Show the leader tag for leaders.
+  const leaderTag = props.member.isLeader ? (
+    <span className="member-teaser__leader-tag"> Club Leader </span>
+  ) : (
+    ''
+  );
+
+  const inheritedClass = props.className ? props.className : '';
+  const classes = [inheritedClass, 'member-teaser'];
+  if (props.member.isLeader) classes.push('member-teaser--leader');
+
   return (
-    <ShadowBox className={props.className}>
-      <div>{preview}</div>
+    <ShadowBox className={classes.join(' ')}>
+      <div> {preview} </div>
       <div
         onClick={handleClick}
         role="button"
@@ -25,20 +52,37 @@ const MemberTeaser = props => {
         onKeyDown={event => {
           returnKeyCheck(event, handleClick);
         }}
+        className="member-teaser__inner"
       >
-        teaser for: {props.member.username}
+        {leaderTag}
+        <div className="member-teaser__image-wrapper">
+          <img src={imageUrl} className="member-teaser__image" alt={name} />
+        </div>
+        <div className="member-teaser__text">
+          <div className="member-teaser__name"> {name} </div>
+          <div className="member-teaser__clubs">
+            <Users />
+            <span> {clubString} </span>
+          </div>
+        </div>
       </div>
     </ShadowBox>
   );
 };
-//
-// export const MemberTeaserShort = (props) => {
-//   return (
-//     <div>
-//       <img src="#" alt="profile" />
-//       <span> {props.title} </span>
-//     </div>
-//   )
-// }
 
 export default MemberTeaser;
+
+//
+// export const query = graphql`
+//   fragment User on OSSNAPI {
+//     userName
+//     firstName
+//     lastName
+//     imageUrl
+//     receiveNewsletter
+//     description
+//     githubUrl
+//     personalUrl
+//     email
+//   }
+// `;
