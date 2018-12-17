68fb1c8bc1842dd19263c601dbc7367f75185654
dpliakos <dimitrispl96@gmail.com>
2018-12-17 17:59:22 +0200

    Remove the mock data from the map.

--- src/components/components/map/map.js
+++ src/components/components/map/map.js
@@ -9,22 +9,19 @@ import './map.scss';
 
 class ClubMap extends React.Component {
   render() {
+    // center the map.
     const position = [38.3345123, -99.5218668];
-    const nevada = [38.0582213, -115.6058512];
-    const phoenix = [42.5, -107.6];
-    const dallas = [37.8155573, -108.4803483];
-
     const clubs = this.props.clubs || [];
-
-    const mockLocations = [
-      { location: nevada, title: 'Club of nevada' },
-      { location: phoenix, title: 'Club of phoenix' },
-      { location: dallas, title: 'Club of Dallas' }
-    ];
 
     const markers = clubs.map((club, i) => {
       return (
-        <Marker position={mockLocations[i].location} key={i}>
+        <Marker
+          position={[
+            club.location || 38.0582213,
+            club.location || -115.6058512
+          ]}
+          key={i}
+        >
           <Popup className="map__popup">
             <ClubTeaser club={club} />
           </Popup>
