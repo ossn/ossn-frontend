932efbe26e59777df999004886800c59d6fa3fd9
iro <iropal14@gmail.com>
2018-12-17 12:43:43 +0200

    Fixes on faw page content and appearance.

--- src/pages/faq-page.js
+++ src/pages/faq-page.js
@@ -52,7 +52,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="I filled out the registration form, what's next? "
-                id="faq-2-item-1"
+                id="faq-1-item-2"
               >
                 Thank you for applying! We have received your application and we
                 invite you to go through the onboarding process in order to get
@@ -60,37 +60,39 @@ const Questions = props => {
                 information about the process here:
                 <ul>
                   <li>
-                    Step 1: Fill out the form to
+                    Step 1: Fill out the form to{' '}
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYQEKz8gMnxWrIxJZer6lJG1p08LtPboV3Aupfj2PtWmKD1A/viewform?usp=sf_link">
-                      register your club
+                      {' '}
+                      register your club.
                     </a>
                   </li>
                   <li>
                     Step 2: Your application is reviewed & you receive a notice
-                    of welcome
+                    of welcome.
                   </li>
                   <li>
                     Step 3: Complete the “Introduction to the network” (coming
-                    soon)
+                    soon).
                   </li>
-                  <li> Step 4: Create your club profile </li>
+                  <li> Step 4: Create your club profile. </li>
                   <li> Step 5: Your club is officially registered! </li>
                   <li>
-                    Step 6: Find great open source
+                    Step 6: Find great open source{' '}
                     <a href="https://opensource.mozilla.community/find-activity/">
                       projects/activities
                     </a>
+                    .
                   </li>
                   <li>
-                    Step 7: Join our regular
+                    Step 7: Join our regular{' '}
                     <a href="https://discourse.mozilla.org/t/demos-calls-for-registered-club-leaders/19343">
                       demos calls
-                    </a>
-                    to meet other club leaders
+                    </a>{' '}
+                    to meet other club leaders.
                   </li>
-                  Remember your club will need to show regular activity in order
-                  to remain in good standing.
                 </ul>
+                Remember your club will need to show regular activity in order
+                to remain in good standing.
               </FaqItem>
             </FaqGroup>
 
@@ -110,13 +112,13 @@ const Questions = props => {
                 id="faq-2-item-2"
               >
                 The goal of the program is to support your club to learn about
-                and contribute to open source projects. You can run any of the
+                and contribute to open source projects. You can run any of the{' '}
                 <a href="https://opensource.mozilla.community/find-activity/">
-                  activities listed in the relevant page on the website
+                  activities listed in the relevant page on the website{' '}
                 </a>
                 or other activities related to open source. If you have an idea
                 for an activity which will advance the mission you can propose
-                it
+                it{' '}
                 <a href="https://github.com/mozilla/Campus-Activities/issues/new">
                   here
                 </a>
@@ -124,7 +126,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="Where can I request swag or budget for my club? "
-                id="faq-3-item-1"
+                id="faq-2-item-3"
               >
                 Budget and swag are not yet available through the Open Source
                 Student Network program. However we recommend checking your
@@ -133,7 +135,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="Do we have to change our name or call ourselves a “Mozilla” Club to be part of the network? "
-                id="faq-3-item-1"
+                id="faq-2-item-4"
               >
                 You do not have to have “Mozilla” in your club name to
                 participate. In fact, we encourage you to keep your name focused
@@ -144,7 +146,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="Can there be multiple clubs registered with Mozilla on the same campus? "
-                id="faq-3-item-1"
+                id="faq-2-item-5"
               >
                 Yes, multiple technical clubs from the same campus can register
                 with Mozilla. For example, on one campus the Women in Computing
@@ -153,7 +155,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="Some clubs would rather exist only virtually, is this possible? "
-                id="faq-3-item-1"
+                id="faq-2-item-6"
               >
                 No. Part of the definition of a club is that they meet regularly
                 in person, while some activities will definitely be virtual, in
@@ -162,7 +164,7 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="What if I’m not a university student but would like to register or join a club? "
-                id="faq-3-item-1"
+                id="faq-2-item-7"
               >
                 If you are passionate about open source and are interested in
                 finding more ways to contribute we encourage you to use all of
@@ -173,13 +175,13 @@ const Questions = props => {
               </FaqItem>
               <FaqItem
                 header="What if I’m not based in the US but would like to register my club? "
-                id="faq-3-item-1"
+                id="faq-2-item-8"
               >
                 This program is currently focused exclusively on the US to test
                 a more focused regional approach to empowering and connecting
                 clubs with a discrete focus on making technical contributions to
                 open source projects. In the meantime we’re continuing to
-                support the work of our non-US student clubs through the
+                support the work of our non-US student clubs through the{' '}
                 <a href="https://campus.mozilla.community/">
                   Campus Clubs program
                 </a>
@@ -190,21 +192,13 @@ const Questions = props => {
           </div>
           <div>
             <FaqGroup id="faq-group-3" header="Creating a club">
-              <FaqItem header="Title 4" id="faq-3-item-1">
-                If you don’t have an established club but would like to start an
-                open source club you are welcome to apply. Fill out the
-                registration form and indicate that you’re starting a new club.
-                We’ll send you materials and tips for launching a successful
-                club.
+              <FaqItem header="Example Faq item" id="faq-3-item-1">
+                Example Faq item
               </FaqItem>
             </FaqGroup>
             <FaqGroup id="faq-group-4" header="Running a club">
-              <FaqItem header="Title 4" id="faq-3-item-1">
-                If you don’t have an established club but would like to start an
-                open source club you are welcome to apply. Fill out the
-                registration form and indicate that you’re starting a new club.
-                We’ll send you materials and tips for launching a successful
-                club.
+              <FaqItem header="Example Faq item" id="faq-4-item-1">
+                Example Faq item
               </FaqItem>
             </FaqGroup>
           </div>
@@ -213,10 +207,9 @@ const Questions = props => {
         <div className="faq__footer">
           <h2 className="faq__footer-title"> Have more questions? </h2>
           <p className="faq__footer-text title title--x-small">
-            Cool! Reach out to us to
-            <a href="https://www.facebook.com/"> Facebook </a>
-            or
-            <a href="https://twitter.com/"> Twitter </a>
+            Cool! Reach out to us to{' '}
+            <a href="https://www.facebook.com/">Facebook</a> or{' '}
+            <a href="https://twitter.com/">Twitter</a>
             <Shape waveLarge sunnyYellow className="faq__footer-shape" />
           </p>
         </div>
