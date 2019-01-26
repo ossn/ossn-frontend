700e9cf1df3ffd9862fa7e395708e03120ac628f
iro <iropal14@gmail.com>
2019-01-26 12:09:50 +0200

    Attemt on user edit.

--- src/templates/member-full.js
+++ src/templates/member-full.js
@@ -3,9 +3,7 @@ A wrapper component for the club full page.
 Is used for member page generator.
 */
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
-import GatsbyConfig from './../../gatsby-config';
 import Member from './../components/components/member/member';
 import BasicLayout from '../components/layouts/layout-base/layout-base';
 
@@ -13,12 +11,6 @@ export default class ClubFullPage extends React.Component {
   render() {
     return (
       <BasicLayout>
-        <Helmet>
-          <title>
-            {['Clubs', '|', GatsbyConfig.siteMetadata.title].join(' ')}
-          </title>
-        </Helmet>
-
         <Member member={this.props.pageContext.member} />
       </BasicLayout>
     );
