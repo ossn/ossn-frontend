eaa3a1a66632605da9bd3276030cd89677791831
dwb17 <david@pointblank.gr>
2019-01-30 13:21:10 +0200

    Issue #116: render marker only if the club has lat and lng

--- src/components/components/map/map.js
+++ src/components/components/map/map.js
@@ -15,17 +15,14 @@ class ClubMap extends React.Component {
 
     const markers = clubs.map((club, i) => {
       return (
-        <Marker
-          position={[
-            club.location || 38.0582213 + i,
-            club.location || -115.6058512 - i
-          ]}
-          key={i}
-        >
-          <Popup className="map__popup">
-            <ClubTeaser club={club} />
-          </Popup>
-        </Marker>
+        // Check that clug has a defined location.
+        club.location && club.location.lat && club.location.lng ? (
+          <Marker position={[club.location.lat, club.location.lng]} key={i}>
+            <Popup className="map__popup">
+              <ClubTeaser club={club} />
+            </Popup>
+          </Marker>
+        ) : null
       );
     });
 
