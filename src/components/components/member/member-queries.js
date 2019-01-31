44fd444f0e95f760f1a6fa443ee424b8ac57fd3c
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-31 16:52:18 +0100

    feat: Fetch member info client side
    fixes #152

---
+++ src/components/components/member/member-queries.js
@@ -0,0 +1,55 @@
+import gql from 'graphql-tag';
+
+export const getUserQuery = gql`
+  query getUser($id: ID!) {
+    user(id: $id) {
+      id
+      userName
+      name
+      imageUrl
+      receiveNewsletter
+      description
+      sortDescription
+      githubUrl
+      personalUrl
+      email
+      clubs {
+        name
+        id
+      }
+    }
+  }
+`;
+
+export const editUserMutation = gql`
+  mutation editUser(
+    $receiveNewsletter: Boolean!
+    $sortDescription: String
+    $description: String
+    $clubs: [ID!]
+    $githubUrl: String
+    $personalUrl: String
+  ) {
+    editUser(
+      user: {
+        receiveNewsletter: $receiveNewsletter
+        sortDescription: $sortDescription
+        description: $description
+        clubs: $clubs
+        githubUrl: $githubUrl
+        personalUrl: $personalUrl
+      }
+    ) {
+      id
+      email
+      sortDescription
+      description
+      receiveNewsletter
+      clubs {
+        id
+      }
+      githubUrl
+      personalUrl
+    }
+  }
+`;
