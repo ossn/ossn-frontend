023f6a0c94499a2871012e74ec35131c0f249c47
iro <iropal14@gmail.com>
2019-02-21 12:26:31 +0200

    Fix: Map toggle text is active.

--- src/components/components/filter/filter.js
+++ src/components/components/filter/filter.js
@@ -1,36 +1,47 @@
 // external modules
-import React from 'react';
-import { Search } from 'react-feather';
+import React from "react";
+import { Search } from "react-feather";
 
 // local modules
-import Toggle from './../toggle/toggle';
+import Toggle from "./../toggle/toggle";
 
 // styles
-import './filter.scss';
+import "./filter.scss";
 
 /*
   Uses a horizontal toggle item and puts their labels.
   The implementation of the logic has to be at an higher level.
+  Active element is considered to be the one on the right.
 */
 export class ToggleFilter extends React.Component {
   handleToggle = this.props.onClick;
 
   render() {
-    const left = this.props.left ? this.props.left : 'left item';
-    const right = this.props.right ? this.props.right : 'right item';
-    const inheritedClass = this.props.className ? this.props.className : '';
-    const classes = [inheritedClass, 'filter', 'filter--toggle'];
+    const left = this.props.left ? this.props.left : "left item";
+    const right = this.props.right ? this.props.right : "right item";
+    const inheritedClass = this.props.className ? this.props.className : "";
+    const classes = [inheritedClass, "filter", "filter--toggle"];
 
     return (
-      <div className={classes.join(' ')}>
-        <span> {left} </span>
+      <div className={classes.join(" ")}>
+        <button
+          onClick={this.props.active && this.handleToggle}
+          className="filter__toggle-text"
+        >
+          {left}
+        </button>
         <Toggle
           onClick={this.handleToggle}
           active={this.props.active}
           left={this.props.left}
           right={this.props.right}
         />
-        <span> {right} </span>
+        <button
+          onClick={!this.props.active && this.handleToggle}
+          className="filter__toggle-text"
+        >
+          {right}
+        </button>
       </div>
     );
   }
@@ -44,12 +55,12 @@ export class SearchFilter extends React.Component {
   handleSearch = this.props.onChange;
 
   render() {
-    const inheritedClass = this.props.className ? this.props.className : '';
-    const classes = [inheritedClass, 'filter', 'filter--search'];
-    const placeholder = this.props.placeholder || 'Search';
+    const inheritedClass = this.props.className ? this.props.className : "";
+    const classes = [inheritedClass, "filter", "filter--search"];
+    const placeholder = this.props.placeholder || "Search";
 
     return (
-      <form className={classes.join(' ')}>
+      <form className={classes.join(" ")}>
         <Search size={18} className="filter__search-icon" />
         <label htmlFor={this.props.id} className="">
           <input
@@ -57,7 +68,7 @@ export class SearchFilter extends React.Component {
             placeholder={placeholder}
             onChange={this.handleSearch}
             onKeyPress={e => {
-              if (e.key === 'Enter') e.preventDefault();
+              if (e.key === "Enter") e.preventDefault();
             }}
             id={this.props.id}
           />
