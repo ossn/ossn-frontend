22ecb936eef047d42b024bb07dde1113d79e5e8d
iro <iropal14@gmail.com>
2019-02-27 17:43:19 +0200

    Turn event into functional component.

--- src/components/components/event/event.js
+++ src/components/components/event/event.js
@@ -6,44 +6,32 @@ import { shortDate } from "./../../../utils/dates";
 import { Calendar as CalendarIcon } from "react-feather";
 
 // Wrapper for events.
-export default class Event extends React.PureComponent {
-  render() {
-    let classes = ["event"];
-    const now = Math.round(new Date().getTime() / 1000);
-    const closingDate = Math.max(
-      this.props.event.startDate,
-      this.props.event.endDate
-    );
-    closingDate < now && classes.push("is-past-event");
-    const startDate = this.props.event.startDate && (
-      <span className="event__date-start">
-        {shortDate(this.props.event.startDate)}
-      </span>
-    );
-    const endDate = this.props.event.endDate &&
-      this.props.event.endDate !== this.props.event.startDate && (
-        <span className="event__date-end">
-          {shortDate(this.props.event.endDate)}
-        </span>
-      );
-    return (
-      <div className={classes.join(" ")}>
-        <div className="event__icon">
-          <CalendarIcon size={20} />
-        </div>
-        <div className="event__text">
-          <div className="event__main">
-            <span className="event__date">
-              {startDate}
-              {endDate}
-            </span>
-            <span className="event__value">{this.props.event.title}</span>
-          </div>
-          <span className="event__secondary">
-            {this.props.event.sortDescription}
-          </span>
-        </div>
+const Event = props => {
+  const now = Math.round(new Date().getTime() / 1000);
+  const { startDate, endDate } = props.event;
+  const closingDate = Math.max(startDate, endDate);
+  const startDateElement = startDate && (
+    <span className="event__date-start">{shortDate(startDate)}</span>
+  );
+  const endDateElement = endDate && endDate !== startDate && (
+    <span className="event__date-end">{shortDate(endDate)}</span>
+  );
+  return (
+    <div className={`event ${closingDate < now ? "is-past-event" : ""}`}>
+      <div className="event__icon">
+        <CalendarIcon size={20} />
       </div>
-    );
-  }
-}
+      <div className="event__text">
+        <div className="event__main">
+          <span className="event__date">
+            {startDateElement}
+            {endDateElement}
+          </span>
+          <span className="event__value">{props.event.title}</span>
+        </div>
+        <span className="event__secondary">{props.event.sortDescription}</span>
+      </div>
+    </div>
+  );
+};
+export default Event;
