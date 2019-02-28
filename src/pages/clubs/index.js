07545664204eeafd7b2472bf22be7724833e6f9f
Ioannis Potouridis <potouridisio@gmail.com>
2019-02-28 10:00:13 +0200

    refactor(clubs-list): write clubs page as function component

--- src/pages/clubs/index.js
+++ src/pages/clubs/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ApolloConsumer, Query } from "react-apollo";
 import { PlusSquare, Search } from "react-feather";
 import { Helmet } from "react-helmet";
@@ -15,23 +15,51 @@ import LayoutContained from "../../components/layouts/layout-contained/layout-co
 import "../../components/pages-styles/find-club.scss";
 import { GET_CLUBS } from "./queries";
 
-class Clubs extends React.PureComponent {
-  state = {
-    cursor: null,
-    firstLoad: true,
-    hasNextPage: false,
-    listClubNumber: 6,
-    number: 6,
-    searchString: null,
-    shownClubs: [],
-    shownClubsCount: 0,
-    view: "list"
+const initialState = {
+  cursor: null,
+  firstLoad: true,
+  hasNextPage: false,
+  listClubNumber: 6,
+  number: 6,
+  searchString: null,
+  shownClubs: [],
+  view: "list"
+};
+
+function Clubs() {
+  const [state, setState] = useState(initialState);
+
+  let timer = 0;
+
+  const content =
+    state.view === "map" ? (
+      <Map clubs={state.shownClubs} />
+    ) : (
+      <ClubTeaserList clubs={state.shownClubs} />
+    );
+
+  const handleSearch = event => {
+    const target = event.target;
+    const value = target.value;
+
+    clearInterval(timer);
+
+    timer = setTimeout(() => {
+      setState({
+        ...state,
+        cursor: null,
+        firstLoad: true,
+        hasNextPage: false,
+        number: state.view === "map" ? 100 : 12,
+        searchString: value,
+        shownClubs: []
+      });
+    }, 300);
   };
 
-  timer = 0;
-
-  handleToggleMap = () => {
-    this.setState(state => ({
+  const handleToggleMap = () => {
+    setState({
+      ...state,
       cursor: null,
       firstLoad: true,
       hasNextPage: false,
@@ -43,53 +71,35 @@ class Clubs extends React.PureComponent {
           : 100,
       shownClubs: [],
       view: state.view === "map" ? "list" : "map"
-    }));
+    });
   };
 
-  handleSearch = event => {
-    const target = event.target;
-    const value = target.value;
-
-    clearInterval(this.timer);
-
-    this.timer = setTimeout(() => {
-      this.setState({
-        cursor: null,
-        firstLoad: true,
-        hasNextPage: false,
-        number: this.state.view === "map" ? 100 : 12,
-        searchString: value,
-        shownClubs: []
-      });
-    }, 300);
-  };
-
-  onClubsFetched = data => {
-    const snapshot = { ...this.state };
+  const onClubsFetched = data => {
+    const snapshot = { ...state };
     const shownClubs = [...snapshot.shownClubs, ...data.clubs.clubs];
 
-    this.setState(() => ({
+    setState({
+      ...state,
       cursor: data.clubs.pageInfo.endCursor,
       firstLoad: snapshot.view === "map" && data.clubs.pageInfo.hasNextPage,
       hasNextPage: data.clubs.pageInfo.hasNextPage,
       listClubNumber:
         snapshot.view === "list" ? shownClubs.length : snapshot.listClubNumber,
       shownClubs: shownClubs
-    }));
+    });
   };
 
-  clubQuery = () => {
-    return this.state.firstLoad ? (
+  const fetchClubs = () =>
+    state.firstLoad && (
       <Query
         query={GET_CLUBS}
         variables={{
-          cursor: this.state.cursor,
-          number: this.state.number,
-          search:
-            this.state.searchString === "" ? null : this.state.searchString
+          cursor: state.cursor,
+          number: state.number,
+          search: state.searchString === "" ? null : state.searchString
         }}
         onCompleted={data => {
-          this.onClubsFetched(data);
+          onClubsFetched(data);
         }}
       >
         {({ loading, error }) => {
@@ -107,94 +117,74 @@ class Clubs extends React.PureComponent {
           }
         }}
       </Query>
-    ) : (
-      ""
     );
-  };
 
-  render() {
-    const {
-      cursor,
-      hasNextPage,
-      number,
-      searchString,
-      shownClubs,
-      view
-    } = this.state;
-
-    const content =
-      view === "map" ? (
-        <Map clubs={shownClubs} />
-      ) : (
-        <ClubTeaserList clubs={shownClubs} />
-      );
-
-    return (
-      <BasicLayout>
-        <Helmet>
-          <title>
-            {["Clubs", "|", GatsbyConfig.siteMetadata.title].join(" ")}
-          </title>
-        </Helmet>
-        <LayoutContained>
-          <div className="find-club__header">
-            <h1> Clubs </h1>
-            <div className="find-club__filters-wrapper">
-              <ShadowBox className="find-club__filters-wrapper-inner">
-                <div className="find-club__filter-toggle">
-                  <ToggleFilter
-                    onClick={this.handleToggleMap}
-                    active={view === "map"}
-                    left="List view"
-                    right="Map View"
-                  />
-                </div>
-                <div className="find-club__filter-search">
-                  <SearchFilter
-                    placeholder="Filter"
-                    label="Filter clubs"
-                    onChange={this.handleSearch}
-                    id="find-club-search"
-                    hideLabel
-                    icon={Search}
-                  />
-                </div>
-              </ShadowBox>
-            </div>
-          </div>
-
-          {content}
-
-          {this.clubQuery()}
-
-          <ApolloConsumer>
-            {client => (
-              <div className="find-club__button-wrapper">
-                <button
-                  className="button button--reset button--icon"
-                  onClick={async () => {
-                    const { data } = await client.query({
-                      query: GET_CLUBS,
-                      variables: {
-                        cursor: cursor,
-                        number: number,
-                        search: searchString === "" ? null : searchString
-                      }
-                    });
-                    this.onClubsFetched(data);
-                  }}
-                  hidden={!hasNextPage}
-                >
-                  <PlusSquare size={16} />
-                  Load more
-                </button>
+  return (
+    <BasicLayout>
+      <Helmet>
+        <title>
+          {["Clubs", "|", GatsbyConfig.siteMetadata.title].join(" ")}
+        </title>
+      </Helmet>
+      <LayoutContained>
+        <div className="find-club__header">
+          <h1> Clubs </h1>
+          <div className="find-club__filters-wrapper">
+            <ShadowBox className="find-club__filters-wrapper-inner">
+              <div className="find-club__filter-toggle">
+                <ToggleFilter
+                  onClick={handleToggleMap}
+                  active={state.view === "map"}
+                  left="List view"
+                  right="Map View"
+                />
               </div>
-            )}
-          </ApolloConsumer>
-        </LayoutContained>
-      </BasicLayout>
-    );
-  }
+              <div className="find-club__filter-search">
+                <SearchFilter
+                  placeholder="Filter"
+                  label="Filter clubs"
+                  onChange={handleSearch}
+                  id="find-club-search"
+                  hideLabel
+                  icon={Search}
+                />
+              </div>
+            </ShadowBox>
+          </div>
+        </div>
+
+        {content}
+
+        {fetchClubs()}
+
+        <ApolloConsumer>
+          {client => (
+            <div className="find-club__button-wrapper">
+              <button
+                className="button button--reset button--icon"
+                onClick={async () => {
+                  const { data } = await client.query({
+                    query: GET_CLUBS,
+                    variables: {
+                      cursor: state.cursor,
+                      number: state.number,
+                      search:
+                        state.searchString === "" ? null : state.searchString
+                    }
+                  });
+                  onClubsFetched(data);
+                }}
+                hidden={!state.hasNextPage}
+              >
+                <PlusSquare size={16} />
+                Load more
+              </button>
+            </div>
+          )}
+        </ApolloConsumer>
+      </LayoutContained>
+    </BasicLayout>
+  );
 }
 
 export default Clubs;
