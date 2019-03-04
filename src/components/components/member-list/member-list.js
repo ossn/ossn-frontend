7ee43d5de085089f11ee73ac30ba8494c45e17ee
iro <iropal14@gmail.com>
2019-03-04 17:48:06 +0200

    Document member list, member teaser, member tools. Wrap member teaser in link..

--- src/components/components/member-list/member-list.js
+++ src/components/components/member-list/member-list.js
@@ -1,24 +1,25 @@
-/*
- List of member-teaser elements
- Prints a list of Members at a two column layout.
- props: members (array of MemberTeaser), the members to be shown
- */
-
 import React from "react";
 
 import Layout2Col from "./../../layouts/layout-2col/layout-2col";
 import MemberTeaser from "./../member-teaser/member-teaser";
 
+/**
+ * Member teaser list.
+ * This component is used for showing a list of members.
+ *
+ * Props contain an the array of members to be shown.
+ *
+ * @param props
+ */
 export default class MemberList extends React.Component {
   getMembers() {
-    const list = this.props.members.map((member, i) => {
+    return this.props.members.map(member => {
       return (
-        <div key={i}>
-          <MemberTeaser member={member} id={i} />
+        <div key={member.id}>
+          <MemberTeaser member={member} />
         </div>
       );
     });
-    return list;
   }
 
   render() {
--- src/components/components/member-tools/member-tools.js
+++ src/components/components/member-tools/member-tools.js
@@ -1,15 +1,18 @@
-/*
-  A wrapper for channels and tools for contributing list.
-  Appears at /contribute and /leaders-corber.
-*/
-
 import React from "react";
 
-// Local modules.
 import LayoutContained from "./../../layouts/layout-contained/layout-contained";
 import Layout2Col from "./../../layouts/layout-2col/layout-2col";
 import { ChannelList } from "./../communication-channels/communication-channels";
 import { ContributingToolSet } from "./../contributing-tools/contributing-tools";
+
+/**
+ * A wrapper for channels and tools for contributing list.
+ * Appears at /contribute and /leaders-corner.
+ *
+ * Props contain channels and tools.
+ *
+ * @param props
+ */
 
 export default props => {
   return (
