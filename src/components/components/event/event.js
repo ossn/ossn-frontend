c99051c11f4d4600fe33d6876f1ef5a0748aa133
iro <iropal14@gmail.com>
2019-03-05 17:04:49 +0200

    Fix: add location to event.

--- src/components/components/event/event.js
+++ src/components/components/event/event.js
@@ -21,6 +21,34 @@ const Event = props => {
   const endDateElement = endDate && endDate !== startDate && (
     <span className="event__date-end">{shortDate(endDate)}</span>
   );
+
+  const location =
+    props.event.location && props.event.location.address ? (
+      props.event.location.lng && props.event.location.lat ? (
+        <span className="event__location">
+          <h4>Event Location</h4>
+          <a
+            className="event__location-text text text--xsmall"
+            href={`https://www.openstreetmap.org/directions?from=&to= ${
+              props.event.location.lat
+            } %2C ${props.event.location.lng} &zoom=12`}
+          >
+            {" "}
+            {props.event.location.address}{" "}
+          </a>
+        </span>
+      ) : (
+        <span className="event__location">
+          <h4>Event Location:</h4>
+          <div className="event__location-text text text--xsmall">
+            {props.event.location.address}
+          </div>
+        </span>
+      )
+    ) : (
+      ""
+    );
+
   return (
     <div className={`event ${closingDate < now ? "is-past-event" : ""}`}>
       <div className="event__icon">
@@ -35,6 +63,7 @@ const Event = props => {
           <span className="event__value">{props.event.title}</span>
         </div>
         <span className="event__secondary">{props.event.sortDescription}</span>
+        {location}
       </div>
     </div>
   );
